c7e821c226da95eedf5fe3003a156ed7b1d2a318
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-09-23 14:56:50 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Portfolio-Site">
+      <header className="Portfolio-Site__Header">
+        <h1>Eliot 
+        <br/>
+        Austin
+        <br/>
+        Forbes
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
