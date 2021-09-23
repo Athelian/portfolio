74e3c884f497439e1f1d7692a0588cdc1e546ff5
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-09-23 19:21:21 +0900

    base layout and cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { Image, Card, Icon, List } from "semantic-ui-react";
+import bitmoji from "Images/bitmoji.png";
+import "semantic-ui-css/semantic.min.css";
+import "./App.sass";
+import about from "Images/About.png";
 
 function App() {
   return (
     <div className="Portfolio-Site">
-      <header className="Portfolio-Site__Header">
-        <h1>Eliot 
-        <br/>
-        Austin
-        <br/>
-        Forbes
+      <header>
+        <h1>
+          <div>
+            Eliot
+            <Image src={bitmoji} circular />
+          </div>
+          Austin-Forbes
         </h1>
       </header>
+      <section className="section section--pink">
+        <img className="section__header" src={about} />
+        <Card.Group centered>
+          <Card
+            header="Rick Sanchez"
+            meta="Scientist"
+            description={[
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+            ].join("")}
+          />
+          <Card
+            header="Rick Sanchez"
+            meta="Scientist"
+            description={[
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+              "Rick is a genius scientist whose alcoholism and reckless,",
+              " nihilistic behavior are a source of concern for his family.",
+            ].join("")}
+          />
+          <Card
+            image={
+              <Card.Content>
+                <Card.Header>
+                  Learner
+                  <Card.Header />
+                </Card.Header>
+                <Card.Meta>Life</Card.Meta>
+                <Icon color="purple" name="book" size="huge" />
+              </Card.Content>
+            }
+            description={
+              <List bulleted>
+                <List.Item>
+                  Eliot was born to David Forbes and Mary Austin at a modest
+                  property in London in 1993.
+                </List.Item>
+                <List.Item>
+                  He then studied at a highschool in Cambridge, before completing a master's degree 
+                </List.Item>
+              </List>
+            }
+          />
+        </Card.Group>
+      </section>
     </div>
   );
 }
