db086846fa44d2d06e91563891216b6168aa0f32
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-03 15:47:25 +0900

    Revert "test tridiv"
    This reverts commit 097a8436975ba5522da357afb63dfa3668f0b108.

--- src/Components/Planets/Planet.js
+++ src/Components/Planets/Planet.js
@@ -1,14 +1,14 @@
-// import "./Planet.sass"
+import "./Planet.sass"
 
-// const Planet = ({variant}) => (
-//   <div className="planet">
-//     <div className="sphere">
-//       <div className="hemisphere">
-//         {variant}
-//       </div>
-//       <div className="hemisphere"></div>
-//     </div>
-//   </div>
-// );
+const Planet = ({variant}) => (
+  <div className="planet">
+    <div className="sphere">
+      <div className="hemisphere">
+        {variant}
+      </div>
+      <div className="hemisphere"></div>
+    </div>
+  </div>
+);
 
-// export default Planet;
+export default Planet;
