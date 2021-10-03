097a8436975ba5522da357afb63dfa3668f0b108
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-03 15:47:19 +0900

    test tridiv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Earth from "Components/Planets/Variants/Earth";
 import about from "Images/about.png";
 import bitmoji from "Images/bitmoji.png";
 import "semantic-ui-css/semantic.min.css";
 import { Card, Icon, Image, List, Popup } from "semantic-ui-react";
-import "./App.sass";
+import "./App.css";
 
 function App() {
   return (
@@ -27,10 +26,408 @@ function App() {
           stacks
         </h3>
       </div>
-      <div>
-        {Earth}
+      <div id="tridiv" className="planet">
+        <div
+          className="scene"
+          style={{
+            WebkitTransform: "rotateX(-20deg) rotateY(-367deg)",
+            msTransform: "rotateX(-20deg) rotateY(-367deg)",
+            transform: "rotateX(-20deg) rotateY(-367deg)",
+          }}
+        >
+          <div className="hemisphere">
+          <>
+            <div className="shape prism-1 pri-1">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face-wrapper rt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face-wrapper lt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+            </div>
+            <div className="shape cuboid-1 cub-1">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face rt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face lt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+              <div className="face tp">
+                <div className="photon-shader" />
+              </div>
+              <div className="cr cr-0">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-1">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-2">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-3">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+            </div>
+            <div className="shape prism-2 pri-2">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face-wrapper rt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face-wrapper lt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+            </div>
+            <div className="shape prism-3 pri-3">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face-wrapper rt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face-wrapper lt">
+                <div className="face">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+            </div>
+            <div className="shape cuboid-2 cub-2">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face rt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face lt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+              <div className="face tp">
+                <div className="photon-shader" />
+              </div>
+              <div className="cr cr-0">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-1">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-2">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-3">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+            </div>
+            <div className="shape cuboid-3 cub-3">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face rt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face lt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+              <div className="face tp">
+                <div className="photon-shader" />
+              </div>
+              <div className="cr cr-0">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-1">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-2">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-3">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+            </div>
+            <div className="shape cuboid-4 cub-4">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face rt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face lt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+              <div className="face tp">
+                <div className="photon-shader" />
+              </div>
+              <div className="cr cr-0">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-1">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-2">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-3">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+            </div>
+            <div className="shape cuboid-5 cub-5">
+              <div className="face ft">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bk">
+                <div className="photon-shader" />
+              </div>
+              <div className="face rt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face lt">
+                <div className="photon-shader" />
+              </div>
+              <div className="face bm">
+                <div className="photon-shader" />
+              </div>
+              <div className="face tp">
+                <div className="photon-shader" />
+              </div>
+              <div className="cr cr-0">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-1">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-2">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+              <div className="cr cr-3">
+                <div className="face side s0">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s1">
+                  <div className="photon-shader" />
+                </div>
+                <div className="face side s2">
+                  <div className="photon-shader" />
+                </div>
+              </div>
+            </div>
+          </>
+          </div>
+        </div>
       </div>
-      <section className="section section--pink">
+      {/* <div>
+        {Earth}
+      </div> */}
+      {/* <section className="section section--pink">
         <img alt="section header" className="section__header" src={about} />
         <Card.Group centered>
           <Card
@@ -106,7 +503,7 @@ function App() {
             }
           />
         </Card.Group>
-      </section>
+      </section> */}
       <section className="section section--white"></section>
     </div>
   );
--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -1,33 +1,33 @@
-import Planet from "../Planet";
-import "./Earth.sass";
+// import Planet from "../Planet";
+// import "./Earth.sass";
 
-const Earth = (
-  <>
-    {Array(2)
-      .fill("")
-      .map((v, i) => (
-        <div className="face face__africa" key={i}>
-          {Array(25)
-            .fill("")
-            .map((v, i) => (
-              <div className="land land__africa" key={i}>
-                <div />
-              </div>
-            ))}
-        </div>
-      ))}
-    {
-      <div className="face face__south_america">
-        {Array(25)
-          .fill("")
-          .map((v, i) => (
-            <div className="land land__south_america" key={i}>
-              <div />
-            </div>
-          ))}
-      </div>
-    }
-  </>
-);
+// const Earth = (
+//   <>
+//     {Array(2)
+//       .fill("")
+//       .map((v, i) => (
+//         <div className="face face__africa" key={i}>
+//           {Array(25)
+//             .fill("")
+//             .map((v, i) => (
+//               <div className="land land__africa" key={i}>
+//                 <div />
+//               </div>
+//             ))}
+//         </div>
+//       ))}
+//     {
+//       <div className="face face__south_america">
+//         {Array(25)
+//           .fill("")
+//           .map((v, i) => (
+//             <div className="land land__south_america" key={i}>
+//               <div />
+//             </div>
+//           ))}
+//       </div>
+//     }
+//   </>
+// );
 
-export default Planet({ variant: Earth });
+// export default Planet({ variant: Earth });
