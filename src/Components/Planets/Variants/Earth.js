0dd7fae47012e3fbd778198a0f1f44027fb95bc0
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-20 23:29:19 +0900

    random positioning

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -2,24 +2,25 @@
  * @Author: Eliot.Austin-Forbes 
  * @Date: 2021-10-19 17:48:05 
  * @Last Modified by: Eliot.Austin-Forbes
- * @Last Modified time: 2021-10-20 16:35:53
+ * @Last Modified time: 2021-10-20 22:44:40
  */
 
 import Planet from "Components/Planets/Planet";
 import VARIABLES from "../../../_variables.module.sass";
 import "./Earth.sass";
 
-const NUMBER_OF_ISLANDS = 4
+const NUMBER_OF_ISLANDS = 6
 const ISLANDS_NO_TILT = [0, 1];
 
 const Earth = () => (
   <Planet>
-    {Array(NUMBER_OF_ISLANDS)
+    {Array(NUMBER_OF_ISLANDS >= 2 ? NUMBER_OF_ISLANDS : 2)
       .fill("")
       .map((_, i) => (
         <div
           className={
             "island" +
+            ` island--${i}` +
             (ISLANDS_NO_TILT.includes(i) ? " island--no_tilt" : "")
           }
           key={i}
