f7adfd6513d1529a7550ec17bb447c19f63faef4
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-21 14:05:55 +0900

    before formula adjustments

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -1,40 +1,52 @@
-/*
- * @Author: Eliot.Austin-Forbes 
- * @Date: 2021-10-19 17:48:05 
- * @Last Modified by: Eliot.Austin-Forbes
- * @Last Modified time: 2021-10-20 22:44:40
- */
-
 import Planet from "Components/Planets/Planet";
 import VARIABLES from "../../../_variables.module.sass";
 import "./Earth.sass";
 
-const NUMBER_OF_ISLANDS = 6
-const ISLANDS_NO_TILT = [0, 1];
+const NUMBER_OF_ISLANDS = 3
+const NUMBER_OF_GLACIERS = 2
 
-const Earth = () => (
-  <Planet>
-    {Array(NUMBER_OF_ISLANDS >= 2 ? NUMBER_OF_ISLANDS : 2)
-      .fill("")
-      .map((_, i) => (
-        <div
-          className={
-            "island" +
-            ` island--${i}` +
-            (ISLANDS_NO_TILT.includes(i) ? " island--no_tilt" : "")
-          }
-          key={i}
-        >
-          {Array(parseInt(VARIABLES.LAYERS_PER_ISLAND))
-            .fill("")
-            .map((v, i) => (
-              <div className="plate" key={i}>
-                <div className="land" />
-              </div>
-            ))}
-        </div>
-      ))}
-  </Planet>
-);
+const getRandomInt = (min, max) => Math.floor(Math.random() * (parseInt(max) - min + 1)) + min
+
+const Earth = () => {
+  let numberOfIslands = NUMBER_OF_ISLANDS
+  const { TOTAL_POSSIBLE_RANDOM_POSITIONS, LAYERS_PER_ISLAND } = VARIABLES
+  const randomNumbers = []
+  while (numberOfIslands) {
+    let random = getRandomInt(NUMBER_OF_GLACIERS, TOTAL_POSSIBLE_RANDOM_POSITIONS)
+    const z = Math.sqrt(20 * (random / 100))
+    const y = Math.sqrt(20 * ((100 - random) / 100))
+    while (randomNumbers.includes(random)) {
+      random = getRandomInt(NUMBER_OF_GLACIERS, TOTAL_POSSIBLE_RANDOM_POSITIONS)
+
+    }
+    randomNumbers.push(random)
+    numberOfIslands--;
+  }
+
+  return (
+    <Planet>
+      {Array(NUMBER_OF_ISLANDS + 2)
+        .fill("")
+        .map((_, i) => {
+          const isGlacier = i < NUMBER_OF_GLACIERS
+          return <div
+            className={
+              "island" +
+              ` island--${isGlacier ? "ice" : randomNumbers.shift()}` +
+              (isGlacier ? " island--no_tilt" : "")
+            }
+            key={i}
+          >
+            {Array(parseInt(LAYERS_PER_ISLAND))
+              .fill("")
+              .map((v, i) => (
+                <div className="plate" key={i}>
+                  <div className="land" />
+                </div>
+              ))}
+          </div>
+        })}
+    </Planet>)
+};
 
 export default Earth;
