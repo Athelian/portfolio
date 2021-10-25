82fdd361d01435f0e8b5d0c10f05445a6bf20f5f
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-25 20:46:26 +0900

    small refactor

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -1,86 +1,29 @@
 import Planet from "Components/Planets/Planet";
-import React, { useState } from "react";
-import { Button } from "semantic-ui-react";
+import React from "react";
 import VARIABLES from "../../../_variables.module.sass";
 import "./Earth.sass";
 
-const NUMBER_OF_CONTINENTS = 1;
-const { LAYERS_PER_ISLAND, TOTAL_POSSIBLE_RANDOM_POSITIONS, ISLAND_DIAMETER } =
-  VARIABLES;
-// it is imperative to spread out the islands evenly over a period of total-random-positions - c / 2,
-// where c is some constant adjusting for the island diameter, as an apex position island will
-// extend beyond the pole somewhat i.e, the island position value is at the extremity of the
-// island not in the center
-const landIndexInterval =
-  (parseInt(TOTAL_POSSIBLE_RANDOM_POSITIONS) - parseInt(ISLAND_DIAMETER) * 2) /
-  2 /
-  NUMBER_OF_CONTINENTS;
-
-const getRandomInt = (min, max) =>
-  Math.floor(Math.random() * (parseInt(max) - min + 1)) + min;
-
+const { LAYERS_PER_ISLAND } = VARIABLES;
 const Earth = () => {
-  // eslint-disable-next-line
-  const [update, setUpdate] = useState(false);
-  const rotations = [];
-
   return (
     <div>
       <Planet variant="earth">
         <div
-          className={
-            "island" +
-            ` island--africa` +
-            ` island--rotate-z-${0}deg`
-          }
+          className={"island" + ` island--africa` + ` island--rotate-z-${0}deg`}
         >
           {Array(parseInt(LAYERS_PER_ISLAND) - 1) // -1 as the final layer has no width or height
             .fill("")
             .map((_, i) => (
-              <div
-                className="plate"
-                key={i}
-                style={
-                  // i !== 31
-                  i < 0
-                    // (i !== 10 && i !== 11 && i !== 12)
-                    ? { contentVisibility: "hidden" }
-                    : null
-                }
-              >
-                <div
-                >
+              <div className="plate" key={i}>
+                <div className="land__window">
                   <div className="land" />
                 </div>
-                <div>
+                <div className="land__window">
                   <div className="land land--rotated" />
                 </div>
               </div>
             ))}
         </div>
-        {/* <div
-          className={
-            "island" +
-            ` island--africa backface_dome` +
-            ` island--rotate-z-${0}deg`
-          }
-        >
-          {Array(parseInt(LAYERS_PER_ISLAND) - 1) // -1 as the final layer has no width or height
-            .fill("")
-            .map((_, i) => (
-              <div
-                className="plate"
-                key={i}
-                style={
-                  i !== 10
-                    ? { contentVisibility: "hidden" }
-                    : null
-                }
-              >
-                <div className="land" />
-              </div>
-            ))}
-        </div> */}
         {/* <div
           className={
             "island" +
@@ -96,29 +39,6 @@ const Earth = () => {
                 key={i}
                 style={
                   i < 20
-                    ? { contentVisibility: "hidden" }
-                    : null
-                }
-              >
-                <div className="land" />
-              </div>
-            ))}
-        </div> */}
-        {/* <div
-          className={
-            "island" +
-            ` island--europe backface_dome` +
-            ` island--rotate-z-${0}deg`
-          }
-        >
-          {Array(parseInt(LAYERS_PER_ISLAND) - 1) // -1 as the final layer has no width or height
-            .fill("")
-            .map((_, i) => (
-              <div
-                className="plate"
-                key={i}
-                style={
-                  i < 20 || i > 40
                     ? { contentVisibility: "hidden" }
                     : null
                 }
