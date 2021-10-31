736f82ef795dca03db8888e62a61cd6a36f751bb
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-10-31 15:04:18 +0900

    small island method start
    many small island refactor
    
    before ce fix
    
    before strip method test
    
    strip method test
    
    Revert "strip method test"
    
    This reverts commit ce7b1cd29b046680a7e567386a27b7cd004619f0.
    
    africa + middle east

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -3,41 +3,28 @@ import React from "react";
 import PlanetProperties from "../PlanetProperties";
 import "./Earth.sass";
 
+console.log(PlanetProperties);
+
 const Earth = () => (
   <div>
     <Planet variant="earth">
       {Object.keys(PlanetProperties["earth"]).map((island) => (
-        <div
-          className={
-            "island" +
-            ` island--${island}` +
-            ` island--rotate-z-${PlanetProperties["earth"][island]["rotation"]}deg`
-          }
-        >
+        <div className={"island" + ` island--${island}`} key={island}>
           {Array(parseInt(PlanetProperties["earth"][island]["layers"]) - 1) // -1 as the final layer has no width or height
             .fill("")
-            .map(
-              (_, i) =>
-                i < 400 && (
-                  <div
-                    className="plate"
-                    key={i}
-                    style={i > 1000 ? { display: "none" } : {}}
-                  >
-                    <div className="land__window">
-                      {Array(
-                        parseInt(
-                          PlanetProperties["earth"][island]["components"]
-                        )
-                      )
-                        .fill("")
-                        .map((_, i) => (
-                          <div className="land" key={i} />
-                        ))}
-                    </div>
-                  </div>
-                )
-            )}
+            .map((_, i) => (
+              <div className="plate" key={i}>
+                <div className="land__window">
+                  {Array(
+                    parseInt(PlanetProperties["earth"][island]?.["components"] || 1)
+                  )
+                    .fill("")
+                    .map((_, i) => (
+                      <div className="land" key={i} />
+                    ))}
+                </div>
+              </div>
+            ))}
         </div>
       ))}
     </Planet>
