ef3769f326f0a25ca41fd2e086b04872b3d5a2fc
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-11-02 14:37:58 +0900

    before size test

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -13,15 +13,23 @@ const Earth = () => (
           {Array(parseInt(PlanetProperties["earth"][island]["layers"]) - 1) // -1 as the final layer has no width or height
             .fill("")
             .map((_, i) => (
-              <div className="plate" key={i}>
+              <div
+                className="plate"
+                key={i}
+              // style={i !== 1 ? { display: "none" } : {}}
+              >
                 <div className="land__window">
+                  {/* <div className="test"> */}
                   {Array(
-                    parseInt(PlanetProperties["earth"][island]?.["components"] || 1)
+                    parseInt(
+                      PlanetProperties["earth"][island]?.["components"] || 1
+                    )
                   )
                     .fill("")
                     .map((_, i) => (
                       <div className="land" key={i} />
                     ))}
+                  {/* </div> */}
                 </div>
               </div>
             ))}
