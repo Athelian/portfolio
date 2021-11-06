d649c5332a868047289d7a44e704145005022722
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-11-06 16:39:08 +0900

    some cloud alignment

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -6,7 +6,9 @@ import VARIABLES from "../../../_variables.module.sass";
 
 const Earth = () => (
   <div className="earth">
-    <div className="earth-container">
+    <div className="earth-container" 
+    style={{ opacity: "0.1" }}
+    >
       <Planet variant="earth">
         {Object.keys(PlanetProperties["earth"]).map((island) => (
           <div className={"island" + ` island--${island}`} key={island}>
