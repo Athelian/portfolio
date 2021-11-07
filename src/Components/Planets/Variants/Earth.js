50938c3feaed6b27b1cac3cdc3003200de4ab981
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-11-07 16:05:29 +0900

    cloud variable mixing and some cpu optimizing

--- src/Components/Planets/Variants/Earth.js
+++ src/Components/Planets/Variants/Earth.js
@@ -4,24 +4,33 @@ import PlanetProperties from "../PlanetProperties";
 import "./Earth.sass";
 import VARIABLES from "../../../_variables.module.sass";
 
-console.log(PlanetProperties)
-console.log(VARIABLES)
+const randomNumber = ({ min, max }) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 const Earth = () => (
   <div className="earth">
-    <div className="earth-container" 
+    <div
+      className="earth-container"
+      // style={{ opacity: 0.1 }}
     >
       <Planet variant="earth">
         {Object.keys(PlanetProperties["earth"]["continents"]).map((island) => (
           <div className={"island" + ` island--${island}`} key={island}>
-            {Array(parseInt(PlanetProperties["earth"]["continents"][island]["layers"]) - 1) // -1 as the final layer has no width or height
+            {Array(
+              parseInt(
+                PlanetProperties["earth"]["continents"][island]["layers"]
+              ) - 1
+            ) // -1 as the final layer has no width or height
               .fill("")
               .map((_, i) => (
                 <div className="plate" key={i}>
                   <div className="surface">
                     {Array(
                       parseInt(
-                        PlanetProperties["earth"]["continents"][island]?.["components"] || 1
+                        PlanetProperties["earth"]["continents"][island]?.[
+                          "components"
+                        ] || 1
                       )
                     )
                       .fill("")
@@ -35,14 +44,34 @@ const Earth = () => (
         ))}
       </Planet>
     </div>
-    <div className="cloud-container">
-      <Planet variant="clouds">
-        {Object.keys(PlanetProperties["earth"]["clouds"]).map((cloud) => (<div className={`island cloud cloud--${cloud}`}>
-          {Array(parseInt(VARIABLES["cloud-layers"]) + 1).fill("").map((_, i) => <div key={i} />)  // Plus one for final layer to be centered
-          } 
-        </div>))}
-      </Planet>
-    </div>
+    {Object.keys(PlanetProperties["earth"]["clouds"]).map((cloud, i) => {
+      const randomZ = randomNumber({ min: -90, max: 90 });
+      const randomY = randomNumber({ min: 0, max: 36 }) * 10;
+      return (
+        <div
+          className={`planet planet--clouds planet--clouds--y-rotation--${randomY}`}
+        >
+          <div className={`sphere`}>
+            <div
+              className="hemisphere"
+              style={{
+                transform: `rotateY(${randomY}deg) rotateZ(${randomZ}deg)`
+              }}
+            >
+              <div className={`island cloud cloud--${cloud}`}>
+                {
+                  Array(parseInt(VARIABLES["cloud-layers"]) + 1)
+                    .fill("")
+                    .map((_, i) => (
+                      <div key={i} />
+                    )) // Plus one for final layer to be centered
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    })}
   </div>
 );
 
