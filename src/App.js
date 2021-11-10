6dca7bc9d1b1d0afa8bf500d4141684831544e0a
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-11-11 01:57:16 +0900

    columns & sections

--- src/App.js
+++ src/App.js
@@ -4,34 +4,40 @@ import "semantic-ui-css/semantic.min.css";
 import { Image, Popup } from "semantic-ui-react";
 import "./App.sass";
 import flaskBook from "Images/flask-book.png";
+import computerHappyFace from "Images/computer-happy-face.png";
 import { useEffect, useRef, useState } from "react";
+import VARIABLES from "./_variables.module.sass";
 
 function App() {
   const titleRef = useRef(null);
-  const [titleInView, setTitleInView] = useState(false);
+  const projectRef = useRef(null);
+  const [aboutInView, setAboutInView] = useState(false);
+  const [projectsInView, setProjectsInView] = useState(false);
 
   useEffect(() => {
-    const options = {
-      root: document.querySelector("#scrollArea"),
-      rootMargin: "0px",
-      threshold: 0.2
-    };
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const [entry] = entries
-        if (entry.isIntersecting) setTitleInView(true)
-        else setTitleInView(false)
-      },
-      options
-    );
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting) setAboutInView(true);
+      else setAboutInView(false);
+    });
     titleRef.current && observer.observe(titleRef.current);
     return () => titleRef.current && observer.unobserve(titleRef.current);
   }, [titleRef]);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting) setProjectsInView(true);
+      else setProjectsInView(false);
+    });
+    projectRef.current && observer.observe(projectRef.current);
+    return () => projectRef.current && observer.unobserve(projectRef.current);
+  }, [projectRef]);
+
   return (
     <div className="Portfolio-Site">
       <div className="stars">
-        {Array(64)
+        {Array(parseInt(VARIABLES["number-of-stars"]))
           .fill("")
           .map(() => (
             <div className="star">
@@ -69,26 +75,68 @@ function App() {
       </header>
       <Earth />
       <section className="section section--light">
-        <h2 className={titleInView ? "slide-out-shadow" : ""} ref={titleRef}>
+        <h2 className={aboutInView ? "slide-out-shadow" : ""} ref={titleRef}>
           About
         </h2>
         <div className="side-container side-container--left-dominant">
           <div className="left">
-            <div className="flag flag--UK"/>
-            <div className="flag flag--JP"/>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            sit amet arcu lacus. Sed enim augue, maximus in tristique vitae,
-            venenatis non orci. Donec tortor sem, maximus scelerisque nulla sed,
-            tristique ultrices justo. Aenean faucibus tempus ex sit amet
-            finibus. Donec mattis viverra nunc vel commodo. Integer ornare felis
-            sed neque ullamcorper, sed scelerisque magna sodales. Maecenas arcu
-            nunc, dapibus ut eleifend et, aliquam ac lacus. Fusce ultrices
-            consequat odio, vitae faucibus arcu volutpat id. Phasellus venenatis
-            venenatis sapien, in rhoncus odio semper at. Curabitur id mauris
-            quis nunc imperdiet bibendum lobortis ac mi.
+            <div>
+              <div className="flag flag--UK" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur sit amet arcu lacus. Sed enim augue, maximus in
+                tristique vitae, venenatis non orci. Donec tortor sem, maximus
+                scelerisque nulla sed, tristique ultrices justo. Aenean faucibus
+                tempus ex sit amet finibus. Donec mattis viverra nunc vel
+                commodo.
+              </p>
+            </div>
+            <div>
+              <div className="flag flag--JP" />
+              <p>
+                Integer ornare felis sed neque ullamcorper, sed scelerisque
+                magna sodales. Maecenas arcu nunc, dapibus ut eleifend et,
+                aliquam ac lacus. Fusce ultrices consequat odio, vitae faucibus
+                arcu volutpat id. Phasellus venenatis venenatis sapien, in
+                rhoncus odio semper at. Curabitur id mauris quis nunc imperdiet
+                bibendum lobortis ac mi.
+              </p>
+            </div>
           </div>
           <div className="right">
             <img src={flaskBook} />
+          </div>
+        </div>
+      </section>
+      <section className="section section--dark">
+        <h2 className={projectsInView ? "slide-out-shadow" : ""} ref={projectRef}>Projects</h2>
+        <div className="side-container side-container--right-dominant">
+          <div className="left">
+            <img src={computerHappyFace} />
+          </div>
+          <div className="right">
+            <div>
+              <div className="flag flag--UK" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur sit amet arcu lacus. Sed enim augue, maximus in
+                tristique vitae, venenatis non orci. Donec tortor sem, maximus
+                scelerisque nulla sed, tristique ultrices justo. Aenean faucibus
+                tempus ex sit amet finibus. Donec mattis viverra nunc vel
+                commodo.
+              </p>
+            </div>
+            <div>
+              <div className="flag flag--JP" />
+              <p>
+                Integer ornare felis sed neque ullamcorper, sed scelerisque
+                magna sodales. Maecenas arcu nunc, dapibus ut eleifend et,
+                aliquam ac lacus. Fusce ultrices consequat odio, vitae faucibus
+                arcu volutpat id. Phasellus venenatis venenatis sapien, in
+                rhoncus odio semper at. Curabitur id mauris quis nunc imperdiet
+                bibendum lobortis ac mi.
+              </p>
+            </div>
           </div>
         </div>
       </section>
