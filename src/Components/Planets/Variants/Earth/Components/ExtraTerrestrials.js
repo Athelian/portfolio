36bd96dc91c3ac1ec04e0ab833e397a3d43fb2e3
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-11-23 16:12:32 +0900

    scrollY normalization for ET

--- src/Components/Planets/Variants/Earth/Components/ExtraTerrestrials.js
+++ src/Components/Planets/Variants/Earth/Components/ExtraTerrestrials.js
@@ -3,20 +3,59 @@ import { useRef } from "react";
 import VARIABLES from "../../../../../_variables.module.sass";
 import "./ExtraTerrestrials.sass";
 
+const initialRotation = parseInt(VARIABLES["starting-rotation"]);
+const slideRotationInterval = parseInt(VARIABLES["slide-rotation-interval"]);
+const numberOfProjects = 4;
+const projectRotationIntervalFactor = 4; // Arbitrary value to increase/decrease the amount of scrolling required per project
+const projectRotationInterval =
+  (projectRotationIntervalFactor * slideRotationInterval) / numberOfProjects;
+
+const getRocketTitle = () =>
+  getComputedStyle(document.documentElement).getPropertyValue("--title");
+const setRocketTitle = (title) =>
+  document.documentElement.style.setProperty("--title", `"${title}"`);
+
 const ExtraTerrestrials = (props) => {
   const { scrollY } = props;
+  const rocketRef = useRef();
+  let slideRotation = initialRotation - scrollY / 8;
+  const projectSlideStart = initialRotation - slideRotationInterval;
+  const projectSlideEnd =
+    initialRotation -
+    slideRotationInterval -
+    numberOfProjects * projectRotationInterval;
+  let rotation = initialRotation - scrollY / 1.59;
 
-  const extraTerrestrialsRef = useRef();
+  console.log("scrolly", scrollY)
 
-  if (extraTerrestrialsRef.current)
-    extraTerrestrialsRef.current.style.transform =
-      "rotateY(" + -scrollY / 2 + "deg)";
+  if (rotation > 0) getRocketTitle() !== "About" && setRocketTitle("About");
+  else if (rotation < 0)
+    getRocketTitle() !== "Projects" && setRocketTitle("Projects");
+  // else if (rotation < 108 + 360) null
+
+  if (slideRotation < projectSlideStart && slideRotation > projectSlideEnd) {
+    rotation = initialRotation - 360;
+  }
+  else if (slideRotation < projectSlideEnd) {
+    // When at the end of the projects
+    // The rotation should be zeroed at that point
+    // Calculate the rotation of this element at projectSlideEnd
+    // when (initialRotation - scrollY / 8) < initialRotation - slideRotationInterval - numberOfProjects * projectRotationInterval
+    // i want cheesecake:) okay<3
+    const scrollYProjectStart = 8 * slideRotationInterval
+    const scrollYProjectEnd = scrollYProjectStart + 8 *(numberOfProjects * projectRotationInterval)
+    const addedScroll = scrollYProjectEnd - scrollYProjectStart
+
+    rotation = initialRotation - (scrollY - addedScroll) / 1.59
+  }
+
+  // console.log(rotation)
 
   return (
     <div className="Earth__Extra-Terrestrials">
       <Planet
         planetClassname="Planet--Extra-Terrestrials"
-        sphereProps={{ ref: extraTerrestrialsRef }}
+        sphereProps={{ style: { transform: `rotateY(${rotation}deg)` } }}
       >
         <div className="Island Island--Rocket">
           {Array(parseInt(VARIABLES["rocket-layers"]))
@@ -26,7 +65,7 @@ const ExtraTerrestrials = (props) => {
                 i === Math.ceil(parseInt(VARIABLES["rocket-layers"]) / 2);
               return (
                 <div className="Rocket">
-                  <div className="Rocket__Inner">
+                  <div className="Rocket__Inner" ref={rocketRef}>
                     <div className="Rocket__Body">
                       {middle && <div className="Rocket__Sail" />}
                     </div>
