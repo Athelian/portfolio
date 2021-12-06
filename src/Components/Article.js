945a877dda1571d439c52760b91210cc05617eae
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-12-06 16:01:49 +0900

    article layout

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,13 +1,15 @@
 import "./Article.sass";
 import Header from "Components/Header"
+import Links from "./Links";
 
 const Article = (props) => {
   const { children } = props;
   return (
-    <>
+    <div className="Article">
       <Header />
-      <div className="Article">{children}</div>
-    </>
+      <div className="Article__Body">{children}</div>
+      <Links/>
+    </div>
   );
 };
 
